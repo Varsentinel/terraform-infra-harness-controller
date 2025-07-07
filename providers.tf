c5a1b53211fed17dff096329af04ef6b1c1e7f73
nylespham <nylespham@gmail.com>
2025-07-07 15:49:44 +0700

    modify

--- providers.tf
+++ providers.tf
@@ -1,17 +1,16 @@
 terraform {
   backend "remote" {
     organization = "Varsentinel"
-
-    workspaces {
-      name = "terraform-infra-harness-controller"
-    }
-
   }
 
   required_providers {
     harness = {
       source  = "harness/harness"
       version = "0.37.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.9.0"
     }
   }
 }
