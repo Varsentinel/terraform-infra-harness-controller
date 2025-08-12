5b7e8f1948250c5ec146e36c7ad8dc81cba896e8
nylespham <nylespham@gmail.com>
2025-08-12 11:14:12 +0700

    VSE-11 Add istio services

--- module/infrastructures/locals.tf
+++ module/infrastructures/locals.tf
@@ -14,6 +14,21 @@ locals {
         "branch"          = "main"
         "import_from_git" = true
       }
+    },
+    {
+      "name"                = "isito-gateway"
+      "identifier"          = "istiogateway"
+      "env_id"              = "vse_dev_apps"
+      "infrastructure_type" = "KubernetesDirect"
+      "deployment_type"     = "NativeHelm"
+      "git_details" = {
+        "store_type"      = "REMOTE"
+        "connector_ref"   = "org.inventium"
+        "repo_name"       = "harness-nextgen-cd"
+        "file_path"       = ".harness/infrastructure/istiogateway.yaml"
+        "branch"          = "main"
+        "import_from_git" = true
+      }
     }
   ]
 }
